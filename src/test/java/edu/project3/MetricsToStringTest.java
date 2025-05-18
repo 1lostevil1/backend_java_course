354676677e9172e3093134e4d35b9722ca77d217
1lostevil1 <146724608+1lostevil1@users.noreply.github.com>
2025-05-19 00:45:10 +0300

    Update MetricsToStringTest.java

--- src/test/java/edu/project3/MetricsToStringTest.java
+++ src/test/java/edu/project3/MetricsToStringTest.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-
+//comment
 public class MetricsToStringTest {
     @Test
     void markdown() throws IOException {
